98e1ba3d0e27215b9f68edc494a57f871a655d9b
Ritik Jaat <ritikjat990@gmail.com>
2024-04-24 13:36:28 +0545

    looks okok

--- Musific/script.js
+++ Musific/script.js
@@ -21,9 +21,10 @@ async function getSongs() {
             const element = as[index];
             // Modify filter condition based on your song link format
             if (element.href.endsWith(".m4a")) {
-                songs.push(element.href);
+                songs.push(element.href.split("/test/")[1]);
             }
         }
+        
         return songs;
     } catch (error) {
         console.error("Error fetching songs:", error);
@@ -32,17 +33,37 @@ async function getSongs() {
     }
 }
 
+const playMusic = (track)=>{
+    let audio = new Audio("/test/" + track)
+    audio.play();
+}
+
 
 async function main() {
+
+    let currentSong;
+
     let songs = await getSongs();
     console.log(songs);
 
-    var audio = new Audio(songs[0]);
-    audio.play();
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUl.innerHTML = songUl.innerHTML + `<li><img class="albumimg" src="/Musific/assets/hplay.svg" alt="">
+                                <div class="info">
+                                    <div>${song.replaceAll("%20", " ")}</div>
+                                    <div></div>
+                                </div>
+                                <img src="/Musific/assets/play.svg" alt="">
+                            </div>
 
-    audio.addEventListener("ontimeupdate", () => {
-        let duration = audio.duration;
-        console.log(duration);
+        </li>`;
+    }
+
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML )
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML)
+        })
     })
 }
 
